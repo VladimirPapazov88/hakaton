dfd3cfb68e5e622cc17e9204e81e3e917818400c
Vladimir Papazov <papazovvladimir88@gmail.com>
2022-12-29 11:00:00 +0300

    svelte flask
    run main.py from server/src

---
+++ server/src/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_from_directory, request
+
+app = Flask(__name__, static_url_path='/../../public')
+
+
+@app.route("/")
+def base():
+    return send_from_directory('../../public', 'index.html')
+
+
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('../../public', path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=8080)
