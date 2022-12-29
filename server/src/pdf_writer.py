8f68c6709eccd6a0c7a3a7a99abe3f9a505efe8d
max <max.taran.210@mail.ru>
2022-12-29 11:41:25 +0300

    pdf

--- server/src/pdf_writer.py
+++ server/src/pdf_writer.py
@@ -1,4 +1,4 @@
-from pypdf import PdfFileWriter, PdfFileReader
+from pypdf import PdfWriter, PdfReader
 import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
@@ -7,13 +7,20 @@ packet = io.BytesIO()
 can = canvas.Canvas(packet, pagesize=letter)
 can.drawString(10, 100, "Hello world")
 can.save()
+
+# move to the beginning of the StringIO buffer
 packet.seek(0)
-new_pdf = PdfFileReader(packet)
-existing_pdf = PdfFileReader(open("original.pdf", "rb"))
-output = PdfFileWriter()
-page = existing_pdf.getPage(0)
-page.mergePage(new_pdf.getPage(0))
-output.addPage(page)
-outputStream = open("destination.pdf", "wb")
+
+# create a new PDF with Reportlab
+new_pdf = PdfReader(packet)
+# read your existing PDF
+existing_pdf = PdfReader(open("train.pdf", "rb"))
+output = PdfWriter()
+# add the "watermark" (which is the new pdf) on the existing page
+page = existing_pdf.pages[0]
+page.merge_page(new_pdf.pages[0])
+output.add_page(page)
+# finally, write "output" to a real file
+outputStream = open("train_out.pdf", "wb")
 output.write(outputStream)
 outputStream.close()
