01ba1015bf94830302885afd10083cfc8091479a
alexander-volodi <alexandervolodin2020@gmail.com>
2022-12-29 11:21:36 +0300

    Add pdf coder

--- server/src/pdf_writer.py
+++ server/src/pdf_writer.py
@@ -1,2 +1,26 @@
 from pypdf import PdfFileWriter, PdfFileReader
 import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+
+packet = io.BytesIO()
+can = canvas.Canvas(packet, pagesize=letter)
+can.drawString(10, 100, "Hello world")
+can.save()
+
+# move to the beginning of the StringIO buffer
+packet.seek(0)
+
+# create a new PDF with Reportlab
+new_pdf = PdfFileReader(packet)
+# read your existing PDF
+existing_pdf = PdfFileReader(open("original.pdf", "rb"))
+output = PdfFileWriter()
+# add the "watermark" (which is the new pdf) on the existing page
+page = existing_pdf.getPage(0)
+page.mergePage(new_pdf.getPage(0))
+output.addPage(page)
+# finally, write "output" to a real file
+outputStream = open("destination.pdf", "wb")
+output.write(outputStream)
+outputStream.close()
