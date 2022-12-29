a5be60ed9d7c3ab87abe785970c55bbc5e7a2c5b
max <max.taran.210@mail.ru>
2022-12-29 14:52:03 +0300

    final backend

--- server/src/main.py
+++ server/src/main.py
@@ -16,7 +16,7 @@ def generate():
     type = request.form.get('type')
     if type == 'certproject':
         type = 'project'
-    if type == 'certproject':
+    if type == 'project':
         pdf_writer.write_project(name, request.form.get('project'))
         img = convert_from_path(pdf_path=f'output/project_{name}.pdf',
                           poppler_path=r'C:\pf\poppler-22.12.0\Library\bin')
